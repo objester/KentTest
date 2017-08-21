8292987b33e5f0423408bf72e9b9d330975eebd3
objester <objester@gmail.com>
2017-08-20 22:45:49 -0500

    no message

---
+++ Kent's Angular Testing/www/js/studentController.js
@@ -0,0 +1,12 @@
+﻿mainApp.controller("studentController", function($scope) { 
+    $scope.student = { 
+        firstName: "", 
+        lastName: "", 
+ 
+        fullName: function() { 
+            var studentObject; 
+            studentObject = $scope.student; 
+            return studentObject.firstName + " " + studentObject.lastName; 
+        } 
+    }; 
+}); 
