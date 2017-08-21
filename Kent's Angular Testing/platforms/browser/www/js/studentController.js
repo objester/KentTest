76c5096982f9fa3271d024060fb463c83d7f59d3
objester <objester@gmail.com>
2017-08-20 23:05:13 -0500

    no message

--- Kent's Angular Testing/platforms/browser/www/js/studentController.js
+++ Kent's Angular Testing/platforms/browser/www/js/studentController.js
@@ -1,15 +1,8 @@
 ﻿mainApp.controller("studentController", function($scope) { 
     $scope.student = { 
-        firstName: "Kent", 
-        lastName: "Matthew", 
-        fees:500, 
-        subjects:[ 
-           {name:'Physics',marks:70}, 
-           {name:'Chemistry',marks:80}, 
-           {name:'Math',marks:65}, 
-           {name:'English',marks:75}, 
-           {name:'Hindi',marks:67} 
-        ], 
+        firstName: "", 
+        lastName: "", 
+ 
         fullName: function() { 
             var studentObject; 
             studentObject = $scope.student; 
